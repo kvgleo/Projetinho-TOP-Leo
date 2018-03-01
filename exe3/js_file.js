f5b58cf6d468606bbd0c8cbad25eeb07cfcf3340
Leonardo Silveira <30756138+kvgleo@users.noreply.github.com>
2018-03-01 19:25:27 -0300

    envio completo

--- exe3/js_file.js
+++ exe3/js_file.js
@@ -1,11 +1,15 @@
 
 function f1(){
 	var text = document.getElementById("txt").value;
-	var list = document.getElementById("list1");
+	document.getElementById("txt").value="";
+	var list = document.getElementById("l_id");
 	var item = document.createElement("li");
-	item.createTextNode(text);
-	//item = "`${text}`";
+	item.textContent = `${text}`;
 		list.appendChild(item);
+
+	item.onclick = function f2(){
+		list.removeChild(item);
+	}
 }
 
 
